5bc1e7b1403d9c557c26a4ff5a39a166a6c7bf3d
opilane <opilane@HP-CZC6297B8V.tthk.ee>
2021-10-20 13:27:12 +0300

    Req2, Req3, Req4 - get print FizzBuzz instead of the number which are multiples of both three and five, get Buzz instead of the number that divide by five and three - refactor

--- src/ee/tthk/tarpv19/iljaHolmogortsev/FizzBuzzTests.java
+++ src/ee/tthk/tarpv19/iljaHolmogortsev/FizzBuzzTests.java
@@ -25,32 +25,26 @@ class FizzBuzzTests {
 	void shouldReturnOneHundredNumbers() { 
 		FizzBuzz fb = new FizzBuzz();
 		int[] expected = IntStream.range(1, 101).toArray();
-		assertArrayEquals(expected, fb.getNumbers());
+		assertArrayEquals(expected, Arrays.stream(fb.getNumbers()).mapToInt(Integer::parseInt).toArray());
 	}
 	
 	@Test
 	void shouldReturnFizzInstedOfNumberThatDividedByThree() {
 		FizzBuzz fb = new FizzBuzz();
-
-		String expected = "1 2 Fizz 4 5 Fizz 7 8 Fizz 10 11 Fizz 13 14 Fizz 16 17 Fizz 19 20 ";
 		
-		assertEquals(expected, fb.getFizz());
-	}
-	@Test
-	void shouldReturnBuzzInstedOfNumberThatDividedByFive() {
-		FizzBuzz fb = new FizzBuzz();
-		String expected = "1 2 3 4 Buzz 6 7 8 9 Buzz 11 12 13 14 Buzz 16 17 18 19 Buzz ";
+		for (int i = 1; i <= fb.getNumbers().length; i++) {
+			if((i % 3) == 0) {
+				assertEquals("Fizz", fb.getFizzBuzz(i));
+			}
+			else if((i % 5) == 0) {
+				assertEquals("Buzz", fb.getFizzBuzz(i));
+			}
+			else if((i % 15) == 0) {
+				assertEquals("FizzBuzz", fb.getFizzBuzz(i));
+			}
+			
+		}
 		
-		assertEquals(expected, fb.getBuzz());
-	}
-	
-	
-	@Test
-	void shouldReturnFizzBuzzInstedOfNumberThatDividedByThreeAndFive() {
-		FizzBuzz fb = new FizzBuzz();
-		String expected = "1 2 FizzBuzz 4 FizzBuzz FizzBuzz 7 8 FizzBuzz FizzBuzz 11 FizzBuzz 13 14 FizzBuzz 16 17 FizzBuzz 19 FizzBuzz ";
-		
-		assertEquals(expected, fb.getFizzBuzz());
 	}
 	
 	
