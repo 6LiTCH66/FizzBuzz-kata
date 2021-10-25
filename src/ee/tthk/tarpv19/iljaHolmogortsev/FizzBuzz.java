de16d9dd732a293aaa6dc554a87127a020b1f513
opilane <opilane@HP-CZC6297B8V.tthk.ee>
2021-10-25 10:03:29 +0300

    Req2, Req3, Req4 - get print FizzBuzz instead of the number which are multiples of both three and five, get Buzz instead of the number that divide by five and three - refactor

--- src/ee/tthk/tarpv19/iljaHolmogortsev/FizzBuzz.java
+++ src/ee/tthk/tarpv19/iljaHolmogortsev/FizzBuzz.java
@@ -1,63 +1,38 @@
+   
 package ee.tthk.tarpv19.iljaHolmogortsev;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FizzBuzz {
 	int[] numbers;
-	public int[] getNumbers() {
+	
+	public String[] getNumbers() {
 		numbers = new int[100];
 		
 		for (int i = 0; i < numbers.length; i++) {
 			numbers[i] = i + 1;
 		}
+
 		
-		return numbers;
+		return Arrays.stream(numbers)
+                .mapToObj(String::valueOf)
+                .toArray(String[]::new);
 	}
 	
 	
-	public String getFizz() {
-		String result = "";
-		for (Integer num: getNumbers().subList(0, 20) ) {
-			if ((num % 3) == 0) {
-				result += "Fizz ";
-			}
-			else {
-				result += num.toString() + " ";
-			}
-			
+	public String getFizzBuzz(int number) {
+		if((number % 3) == 0) {
+			return "Fizz";
+		}
+		else if((number % 5) == 0) {
+			return "Buzz";
+		}
+		else if((number % 15) == 0) {
+			return "FizzBuzz";
 		}
 		
-		return result;
-	}
-	
-	public String getBuzz() {
-		String result = "";
-		for (Integer num: getNumbers().subList(0, 20) ) {
-			if ((num % 5) == 0) {
-				result += "Buzz ";
-			}
-			else {
-				result += num.toString() + " ";
-			}
-			
-		}
-		return result;
-	}
-	
-	public String getFizzBuzz() {
-		String result = "";
-		for (Integer num: getNumbers().subList(0, 20) ) {
-			if ((num % 3) == 0) {
-				result += "FizzBuzz ";
-			}
-			else if((num % 5) == 0) {
-				result += "FizzBuzz ";
-			}
-			else {
-				result += num.toString() + " ";
-			}
-		}
-		return result;
+		return String.valueOf(number);
 	}
 
 }
